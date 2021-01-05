04b628763bef3f271be55f107423086dadbedcb1
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-01-05 12:38:27 -0800

    resolve error for id request

--- routes/postsController.js
+++ routes/postsController.js
@@ -1,49 +1,48 @@
-const expresse=require('express'); 
-const router=expresse.Router();
-const ObjectID=require('mongoose').Types.ObjectId;
+const expresse = require('express');
+const router = expresse.Router();
+const ObjectID = require('mongoose').Types.ObjectId;
 
-const {PostsModels}=require('../models/postsModels')
+const { PostsModels } = require('../models/postsModels')
 
-router.get("/",(req,res)=>{
-    PostsModels.find((err,docs)=>{
-        if (!err)res.send(docs);
+router.get("/", (req, res) => {
+    PostsModels.find((err, docs) => {
+        if (!err) res.send(docs);
         else console.log("Error file" + err);
     });
 });
 
 
-router.post("/",(req,res)=>{
-    console.log(req.body );
-    const newRecord=PostsModels({
-        author:req.body.author,
-        message:req.body.message
+router.post("/", (req, res) => {
+    console.log(req.body);
+    const newRecord = PostsModels({
+        author: req.body.author,
+        message: req.body.message
     });
-    newRecord.save((err,docs)=>{
+    newRecord.save((err, docs) => {
         if (!err) res.send(docs)
-        else console.log("Erreur d'enregistrement : "+err);
-            
+        else console.log("Erreur d'enregistrement : " + err);
+
     });
 });
 
 //modifier le donne
-router.put("/:id",(req,res)=>{
-    if(!ObjectID.isValid(req.params.id))
-        return res.status(400).send("Error Id Unknows"+ res.params.id )
-     const updateRecord={
-         author:req.body.author,
-         message:req.body.message
-     };
-    
-     PostsModels.findByIdAndUpdate(
-        req.params.id, 
-        {$set:updateRecord},
-        {new:true},
-        (err,docs)=>{   
-            if(!err)res.send(docs)
-            else console.log("Error de modification"+err);
-   
-    });
-     
+router.put("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) 
+       return res.status(400).send("Error Id Unknows " + req.params.id);
+
+    const updateRecord = {
+        author: req.body.author,
+        message: req.body.message
+    };
+
+    PostsModels.findByIdAndUpdate(
+        req.params.id,
+        { $set: updateRecord },
+        { new: true },
+        (err, docs) => {
+            if (!err) res.send(docs)
+            else console.log("Error de modification" + err);
+        });
 });
 
-module.exports=router;
+module.exports = router;
