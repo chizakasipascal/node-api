e6e30597db35122b50df291ddfe6129bac97b4e9
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-01-05 10:36:40 -0800

    method de mofification de donne

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const expresse=require('express');
 const app=expresse();
+const bodyParser=require('body-parser');
+
 require ('./services/dbSerices')
 
-
 const postsRoutes=require('./routes/postsController'); 
-app.use('/',postsRoutes);
+app.use(bodyParser.json());
+app.use('/posts',postsRoutes);
 
 
 app.listen(2700 ,()=>{
     console.log("Server stated to port 2700");
 });
+ 
--- routes/postsController.js
+++ routes/postsController.js
@@ -1,12 +1,33 @@
-const expresse=require('express');
+const expresse=require('express'); 
 const router=expresse.Router();
+const ObjectID=require('mongoose').Types.ObjectId;
 
 const {PostsModels}=require('../models/postsModels')
 
 router.get('/',(req,res)=>{
     PostsModels.find((err,docs)=>{
-        console.log(docs);
+        if (!err)res.send(docs);
+        else console.log("Error file" + err);
     });
 });
 
-module.exports=router
+
+router.post('/',(req,res)=>{
+    console.log(req.body );
+    const newRecord=PostsModels({
+        author:req.body.author,
+        message:req.body.message
+    });
+    newRecord.save((err,docs)=>{
+        if (!err) res.send(docs)
+        else console.log("Erreur d'enregistrement : "+err);
+            
+    });
+});
+
+//modifier le donne
+router.put('/:id',(req,res)=>{
+
+});
+
+module.exports=router;
